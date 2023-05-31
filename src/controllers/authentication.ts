d6f23adbd75fdbb238265f972dfae5cbda814d20
Petya Angelova <bedfordpetya@gmail.com>
2023-05-31 23:38:48 +0300

    user login added

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,8 @@ import express = require('express');
 import { getUserByEmail, createUser } from '../db/users';
 import { random, authentication } from '../helpers';
 
+
+//user registration
 export const register = async (req: express.Request, res: express.Response) => {
     try {
         const { email, password, username } = req.body;
@@ -31,3 +33,39 @@ export const register = async (req: express.Request, res: express.Response) => {
         return res.sendStatus(400);
     }
 };
+
+
+//user login
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.sendStatus(400);
+        }
+        //check if user exists
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+        if (!user) {
+            return res.sendStatus(404);
+        }
+        //check if password is correct
+        const expectedHash = authentication(user.authentication.salt, password);
+        if (user.authentication.password !== expectedHash) {
+            return res.sendStatus(403);
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+        await user.save();
+
+        res.cookie('sessionToken', user.authentication.sessionToken, {
+            domain: 'localhost',
+            path: '/',
+        });
+
+        return res.status(200).json(user).end();
+
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(400);
+    }
+}
--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,9 +1,10 @@
 import express = require('express');
 
-import { register } from '../controllers/authentication';
+import { register, login } from '../controllers/authentication';
 
 export default (router: express.Router) => {
     router.post('/auth/register', register);
+    router.post('/auth/login', login);
 
 };
 
